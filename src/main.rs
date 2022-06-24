42f0a39c1ddb27e9dd1aef6e120bd1b3b7eb3c1c
df@ <@>
2022-06-24 18:29:41 +0100

    signle task duration tracking works.

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,42 @@
 use std::time::{Instant, Duration};
 
+enum Status {
+    running(Instant),
+    idle,
+}
+
 struct Task {
-    last_start_time: Instant,
+    last_start_time: Status,
     logged_time: Duration,
+}
+
+impl Status {
+    fn to_duration(&self) -> Duration {
+        match self {
+            Self::running(start_time) => start_time.elapsed(),
+            Self::idle => Duration::new(0, 0),
+        }
+    }
 }
 
 impl Task {
     fn new() -> Self {
         Self {
-            last_start_time: Instant::now(),
+            last_start_time: Status::running(Instant::now()),
             logged_time: Duration::new(0, 0),
         }
+    }
+
+    fn start(&mut self) {
+
+    }
+
+    fn stop(&mut self) {
+
+    }
+
+    fn elapsed_time(&self) -> Duration {
+        self.logged_time + self.last_start_time.to_duration()
     }
 }
 
@@ -20,4 +46,14 @@ fn main() {
     println!("{:#?}", t0.elapsed());
     let k = t0.elapsed() + Duration::from_secs(1);
     println!("{:?}", k);
+
+    let task1 = Task::new();
+    std::thread::sleep(Duration::new(1, 0));
+    println!("task1: {:?}", task1.elapsed_time());
+    std::thread::sleep(Duration::new(1, 0));
+    println!("task1: {:?}", task1.elapsed_time());
+    std::thread::sleep(Duration::new(1, 0));
+    println!("task1: {:?}", task1.elapsed_time());
+
+
 }
