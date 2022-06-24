483a2298bff2c6e137d490b98807d06943f903e2
df@ <@>
2022-06-24 18:16:54 +0100

    some time / duration rnd

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::time::{Instant, Duration};
+
+struct Task {
+    last_start_time: Instant,
+    logged_time: Duration,
+}
+
+impl Task {
+    fn new() -> Self {
+        Self {
+            last_start_time: Instant::now(),
+            logged_time: Duration::new(0, 0),
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let t0 = Instant::now();
+    std::thread::sleep(Duration::new(1, 0));
+    println!("{:#?}", t0.elapsed());
+    let k = t0.elapsed() + Duration::from_secs(1);
+    println!("{:?}", k);
 }
