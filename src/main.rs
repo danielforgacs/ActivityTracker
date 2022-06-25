89ad9dfc7057dce13cd4adabdc16ce69e2bab2a8
df@ <@>
2022-06-25 16:24:52 +0100

    started server

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,16 @@
-fn main() {}
+use actix_web::{App, HttpServer};
+
+const ADDRESS: &str = "127.0.0.1";
+const PORT: u16 = 8000;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()>{
+    println!("Starting: http://{}:{}", ADDRESS, PORT);
+    HttpServer::new(|| {
+        App::new()
+    })
+    .bind((ADDRESS, PORT))?
+    .workers(4)
+    .run()
+    .await
+}
