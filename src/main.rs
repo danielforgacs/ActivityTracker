e93464d708439182031ba5995b066a72c5521015
df@ <@>
2022-06-25 15:12:52 +0100

    turned off main, going with tests

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 mod structs;
 
-use std::time::{Instant, Duration};
-use structs::task::Task;
+// use std::time::{Instant, Duration};
+// use structs::task::Task;
 
 fn main() {
     // let t0 = Instant::now();
@@ -10,27 +10,27 @@ fn main() {
     // let k = t0.elapsed() + Duration::from_secs(1);
     // println!("{:?}", k);
 
-    let mut task1 = Task::new("adasdfasfd");
-    std::thread::sleep(Duration::new(1, 0));
-    println!("task1: {:?} - expected: 1", task1.elapsed_time());
-    std::thread::sleep(Duration::new(1, 0));
-    println!("task1: {:?} - expected: 2", task1.elapsed_time());
-    std::thread::sleep(Duration::new(1, 0));
-    println!("task1: {:?} - expected: 3", task1.elapsed_time());
-    task1.stop();
-    std::thread::sleep(Duration::new(1, 0));
-    println!("task1: {:?} - stopped", task1.elapsed_time());
-    std::thread::sleep(Duration::new(1, 0));
-    println!("task1: {:?} - stopped", task1.elapsed_time());
-    task1.start();
-    std::thread::sleep(Duration::new(1, 0));
-    println!("task1: {:?} - expected: 4", task1.elapsed_time());
-    task1.start();
-    println!("task1: {:?} - expected: 4", task1.elapsed_time());
-    task1.start();
-    println!("task1: {:?} - expected: 4", task1.elapsed_time());
-    task1.start();
-    println!("task1: {:?} - expected: 4", task1.elapsed_time());
-    task1.start();
-    println!("task1: {:?} - expected: 4", task1.elapsed_time());
+    // let mut task1 = Task::new("adasdfasfd");
+    // std::thread::sleep(Duration::new(1, 0));
+    // println!("task1: {:?} - expected: 1", task1.elapsed_time());
+    // std::thread::sleep(Duration::new(1, 0));
+    // println!("task1: {:?} - expected: 2", task1.elapsed_time());
+    // std::thread::sleep(Duration::new(1, 0));
+    // println!("task1: {:?} - expected: 3", task1.elapsed_time());
+    // task1.stop();
+    // std::thread::sleep(Duration::new(1, 0));
+    // println!("task1: {:?} - stopped", task1.elapsed_time());
+    // std::thread::sleep(Duration::new(1, 0));
+    // println!("task1: {:?} - stopped", task1.elapsed_time());
+    // task1.start();
+    // std::thread::sleep(Duration::new(1, 0));
+    // println!("task1: {:?} - expected: 4", task1.elapsed_time());
+    // task1.start();
+    // println!("task1: {:?} - expected: 4", task1.elapsed_time());
+    // task1.start();
+    // println!("task1: {:?} - expected: 4", task1.elapsed_time());
+    // task1.start();
+    // println!("task1: {:?} - expected: 4", task1.elapsed_time());
+    // task1.start();
+    // println!("task1: {:?} - expected: 4", task1.elapsed_time());
 }
