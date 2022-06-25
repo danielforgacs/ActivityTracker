04672b7324866003005223f8e2d6235ac7a97430
df@ <@>
2022-06-25 13:05:45 +0100

    taskmanager basics;

--- src/structs/taskmanager.rs
+++ src/structs/taskmanager.rs
@@ -4,15 +4,28 @@ use super::task::Task;
 struct TaskManager {
     tasks: Vec<Task>,
 }
- impl TaskManager {
+
+impl TaskManager {
     fn new() -> Self {
         Self { tasks: Vec::new() }
     }
- }
+
+    fn new_task(&mut self, name: &str) {
+
+    }
+
+    fn stop_task(&mut self, name: &str) {
+
+    }
+
+    fn stop_all(&mut self) {
+
+    }
+}
 
 
- #[cfg(test)]
- mod test {
+#[cfg(test)]
+mod test {
     use super::*;
 
     #[test]
@@ -20,4 +33,11 @@ struct TaskManager {
         let tm = TaskManager::new();
         assert_eq!(tm.tasks, Vec::new());
     }
- }
+
+    #[test]
+    fn add_task() {
+        let mut tm = TaskManager::new();
+        let task_name = "task";
+        tm.new_task(task_name);
+    }
+}
