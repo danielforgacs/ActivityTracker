c7e30d6a236440d0ddbb8743109b667d3769105f
df@ <@>
2022-06-25 15:10:35 +0100

    fixed task manager

--- src/structs/taskmanager.rs
+++ src/structs/taskmanager.rs
@@ -13,15 +13,26 @@ impl TaskManager {
     fn new_task(&mut self, name: &str) {
         self.tasks.push(
             Task::from(name)
-        )
+        );
+        self.start_task(name);
     }
 
     fn stop_task(&mut self, name: &str) {
-
+        for task in self.tasks.iter_mut() {
+            if task.name() == name {
+                task.stop();
+            }
+        }
     }
 
     fn start_task(&mut self, name: &str) {
-
+        for task in self.tasks.iter_mut() {
+            if task.name() == name {
+                task.start();
+            } else {
+                task.stop();
+            }
+        }
     }
 
     fn stop_all(&mut self) {
@@ -67,5 +78,45 @@ mod test {
         assert_eq!(tm.tasks[0].elapsed_time(), 2);
         std::thread::sleep(std::time::Duration::from_secs(1));
         assert_eq!(tm.tasks[0].elapsed_time(), 3);
+    }
+
+    #[test]
+    fn multiple_tasks() {
+        let task_1 = "alpha";
+        let task_2 = "beta";
+        let mut tm = TaskManager::new();
+        tm.new_task(task_1);
+        pause();
+        assert_eq!(tm.tasks[0].elapsed_time(), 1);
+        tm.new_task(task_2);
+        pause();
+        assert_eq!(tm.tasks[0].elapsed_time(), 1);
+        assert_eq!(tm.tasks[1].elapsed_time(), 1);
+        pause();
+        assert_eq!(tm.tasks[0].elapsed_time(), 1);
+        assert_eq!(tm.tasks[1].elapsed_time(), 2);
+        pause();
+        assert_eq!(tm.tasks[0].elapsed_time(), 1);
+        assert_eq!(tm.tasks[1].elapsed_time(), 3);
+        tm.start_task(task_2);
+        assert_eq!(tm.tasks[0].elapsed_time(), 1);
+        assert_eq!(tm.tasks[1].elapsed_time(), 3);
+        pause();
+        assert_eq!(tm.tasks[0].elapsed_time(), 1);
+        assert_eq!(tm.tasks[1].elapsed_time(), 4);
+        tm.start_task(task_1);
+        assert_eq!(tm.tasks[0].elapsed_time(), 1);
+        assert_eq!(tm.tasks[1].elapsed_time(), 4);
+        pause();
+        assert_eq!(tm.tasks[0].elapsed_time(), 2);
+        assert_eq!(tm.tasks[1].elapsed_time(), 4);
+        pause();
+        assert_eq!(tm.tasks[0].elapsed_time(), 3);
+        assert_eq!(tm.tasks[1].elapsed_time(), 4);
+    }
+
+    fn pause() {
+        let pause_secs = 1;
+        std::thread::sleep(std::time::Duration::from_secs(pause_secs));
     }
 }
