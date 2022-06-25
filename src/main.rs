9fbcd1805e2217c739e710550d25acc0c9586ab0
df@ <@>
2022-06-25 16:43:30 +0100

    Revert ".."
    This reverts commit 555bca2a4e86ccf2d0762bf516985014c6e2f97b.

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,11 @@
-mod structs;
-mod views;
-
-use std::sync::Mutex;
-use actix_web::{App, HttpServer, web::Data};
+use actix_web::{App, HttpServer};
 
 const ADDRESS: &str = "127.0.0.1";
 const PORT: u16 = 8000;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()>{
-    let task_manager = Data::new(
-
-    )
-    println!("serving http://{}:{}", ADDRESS, PORT);
+    println!("Starting: http://{}:{}", ADDRESS, PORT);
     HttpServer::new(|| {
         App::new()
     })
