c59af1ac2a136e3236dd89014bb7636e8370e049
df@ <@>
2022-06-27 18:50:29 +0100

    clean up

--- src/api_views/views.rs
+++ src/api_views/views.rs
@@ -2,7 +2,6 @@ use crate::{TaskManager, taskmanager_as_string};
 use actix_web::{HttpRequest, get, HttpResponse, Result, Responder};
 use actix_web::web::{Path, Data, Json};
 use std::sync::Mutex;
-use serde_json;
 
 #[get("{name}/start")]
 pub async fn start_activity(name: Path<String>, req: HttpRequest) -> HttpResponse {
@@ -34,11 +33,5 @@ pub async fn times(req: HttpRequest) -> Result<impl Responder> {
     let data = req.app_data::<Data<Mutex<TaskManager>>>().unwrap();
     let tm = data.lock().unwrap().clone();
     println!("{}", taskmanager_as_string(&tm));
-    // let r = tm.clone();
-    // let r = serde_json::to_string_pretty(&r).unwrap();
     Ok(Json(tm))
-    // HttpResponse::Ok()
-    //     .body(
-    //         tm.times()
-    //     )
 }
