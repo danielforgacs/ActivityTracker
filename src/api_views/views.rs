3983671275ea8f16e0b7fdbcc7e2885d51627212
df@ <@>
2022-06-27 18:21:23 +0100

    views now return http response.

--- src/api_views/views.rs
+++ src/api_views/views.rs
@@ -1,32 +1,41 @@
-use actix_web::{HttpRequest, get};
+use actix_web::{HttpRequest, get, HttpResponse};
 use actix_web::web::{Path, Data};
 
 use crate::{TaskManager, taskmanager_as_string};
 use std::sync::Mutex;
 
 #[get("{name}/start")]
-pub async fn start_activity(name: Path<String>, req: HttpRequest) -> String {
+pub async fn start_activity(name: Path<String>, req: HttpRequest) -> HttpResponse {
     let data = req.app_data::<Data<Mutex<TaskManager>>>().unwrap();
     let mut tm = data.lock().unwrap();
     tm.activate(&name);
-    format!("activated task: {} Ok.", name)
+    HttpResponse::Ok()
+        .body(
+            format!("activated task: {} Ok.", name)
+        )
 }
 
 #[get("{name}/stop")]
-pub async fn stop_activity(name: Path<String>, req: HttpRequest) -> String {
+pub async fn stop_activity(name: Path<String>, req: HttpRequest) -> HttpResponse {
     let data = req.app_data::<Data<Mutex<TaskManager>>>().unwrap();
     let mut tm = data.lock().unwrap();
-    if tm.stop(&name) {
-        format!("stopped task: {} Ok.", name)
-    } else {
-        format!("error. can't stop task: {}.", name)
-    }
+    HttpResponse::Ok()
+        .body(
+            if tm.stop(&name) {
+                format!("stopped task: {} Ok.", name)
+            } else {
+                format!("error. can't stop task: {}.", name)
+            }
+        )
 }
 
 #[get("times")]
-pub async fn times(req: HttpRequest) -> String {
+pub async fn times(req: HttpRequest) -> HttpResponse {
     let data = req.app_data::<Data<Mutex<TaskManager>>>().unwrap();
     let tm = data.lock().unwrap();
     println!("{}", taskmanager_as_string(&tm));
-    tm.times()
+    HttpResponse::Ok()
+        .body(
+            tm.times()
+        )
 }
