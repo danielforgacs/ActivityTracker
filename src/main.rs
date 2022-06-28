f141a9a4cc10faf87c42ca234f56504f76d2cf06
df@ <@>
2022-06-28 13:01:40 +0100

    version

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use structs::taskmanager::{TaskManager};
 use api_views::views::*;
 use std::sync::Mutex;
 
-const VERSION: &str = "2022.6.27";
+const VERSION: &str = "2022.6.28";
 const ABOUT: &str = r#"
 Web app to track time spent on activities.
 
