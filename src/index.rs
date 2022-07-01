6237ed4d68924b92fa462faefedf60776d5eb99e
df@ <@>
2022-07-01 19:38:40 +0100

    template works

--- src/index.rs
+++ src/index.rs
@@ -1,7 +1,7 @@
 use actix_web::{HttpRequest, get, HttpResponseBuilder, http, HttpResponse};
 use tera::{Tera, Context};
 
-const INDEX_TEMPLATE: &str = "
+const INDEX_TEMPLATE: &str = r#"
 <!DOCTYPE html>
 <html>
 <head>
@@ -10,13 +10,15 @@ const INDEX_TEMPLATE: &str = "
     <title>Page Title</title>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
     <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
-    <script src='main.js'></script>
+    <script>
+        console.log("hey, how cool is this!")
+    </script>
 </head>
 <body>
 {{ message }}
 </body>
 </html>
-";
+"#;
 
 #[get("/")]
 async fn index_view(req: HttpRequest) -> HttpResponse {
@@ -24,7 +26,7 @@ async fn index_view(req: HttpRequest) -> HttpResponse {
     let template_name = "something-cool.html";
     tera.add_raw_template(template_name, INDEX_TEMPLATE).unwrap();
     let mut ctx = Context::new();
-    ctx.insert("message", "fokkin works");
+    ctx.insert("message", "Coming from Rust! Check the console!");
     let render = tera.render(template_name, &ctx).unwrap();
     HttpResponse::Ok()
         .body(render)
