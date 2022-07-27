8a72a627e3a10db9e1d2b2c0f1b25a64b5becdfe
df@ <@>
2022-07-27 01:02:21 +0100

    index.html location is now <binary path>/static/index.html

--- src/client_views/index.rs
+++ src/client_views/index.rs
@@ -3,7 +3,10 @@ use actix_files::{NamedFile};
 
 #[get("/")]
 async fn index_view(req: HttpRequest) -> HttpResponse {
-    match NamedFile::open("static/index.html") {
+    let exe_path = std::env::current_exe().unwrap();
+    let current_dir = exe_path.parent().unwrap();
+    let index_path = format!("{}/{}", current_dir.display(), "static/index.html");
+    match NamedFile::open(index_path) {
         Ok(named_file) => named_file.into_response(&req),
         Err(error) => HttpResponse::Ok().body(
             format!("Error with index.html: {}", error.to_string())
